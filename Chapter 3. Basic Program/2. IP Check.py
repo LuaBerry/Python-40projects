e9119e23aee70c26cb1038e70178bb172eca6f2e
나란히 <lazpberry1012@gmail.com>
2022-06-05 22:55:56 +0900

    Project 2. IP Check

---
+++ Chapter 3. Basic Program/2. IP Check.py
@@ -0,0 +1,13 @@
+import socket
+import requests
+import re
+
+in_addr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+in_addr.connect(("www.google.co.kr", 443))
+print("Internal IP: ", in_addr.getsockname()[0])
+
+
+
+req = requests.get("http://ipconfig.kr")
+out_addr = re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]
+print("External IP: ", out_addr)
